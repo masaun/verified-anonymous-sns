726d284888101b22225bd330efa9886de800dcc8
masaun <masaun@protonmail.com>
2025-07-27 21:38:57 +0900

    [circuit]: Modify the location, where a verified_anonymous_sns_jwt.json is copied to

--- circuit/zkjwt_circuit/build.sh
+++ circuit/zkjwt_circuit/build.sh
@@ -18,15 +18,17 @@ echo "Gate count:"
 bb gates -b target/verified_anonymous_sns_jwt.json | jq  '.functions[0].circuit_size'
 
 # Create version-specific directory
-mkdir -p "../app/assets/jwt"
+#mkdir -p "../app/assets/jwt"
 
-echo "Copying circuit.json to app/assets/jwt..."
-cp target/verified_anonymous_sns_jwt.json "../app/assets/jwt/circuit.json"
+echo "Copying verified_anonymous_sns_jwt.json to ./circuit..."
+#echo "Copying circuit.json to app/assets/jwt..."
+cp target/verified_anonymous_sns_jwt.json "../verified_anonymous_sns_jwt.json"
+#cp target/verified_anonymous_sns_jwt.json "../app/assets/jwt/circuit.json"
 
 echo "Generating vkey..."
 bb write_vk -b ./target/verified_anonymous_sns_jwt.json -o ./target
 
-echo "Generating vkey.json to app/assets/jwt..."
-node -e "const fs = require('fs'); fs.writeFileSync('../app/assets/jwt/circuit-vkey.json', JSON.stringify(Array.from(Uint8Array.from(fs.readFileSync('./target/vk')))));"
+#echo "Generating vkey.json to app/assets/jwt..."
+#node -e "const fs = require('fs'); fs.writeFileSync('../app/assets/jwt/circuit-vkey.json', JSON.stringify(Array.from(Uint8Array.from(fs.readFileSync('./target/vk')))));"
 
 echo "Done"
